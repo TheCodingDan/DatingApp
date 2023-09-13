3f08285590d7a2f9e07afa8dea0197747eff178c
TheCodingDan <dancgr2023@gmail.com>
2023-09-13 17:40:44 -0300

    End of section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -13,6 +13,7 @@ namespace API.Helpers
             .ForMember(d => d.PhotoUrl, opt => opt.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url))
             .ForMember(d => d.Age, opt => opt.MapFrom(s => s.DateOfBirth.CalculeteAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
         }
         
     }
